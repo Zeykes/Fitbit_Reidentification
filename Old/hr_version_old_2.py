f1cb5efbef66bbb6d61fa5a78def388ec229cdce
Martin <martinkristoffer@gmail.com>
2020-10-31 22:12:38 +0100

    Correct uploaded files

--- Old/hr_version_old_2.py
+++ Old/hr_version_old_2.py
@@ -3,15 +3,15 @@ import pandas as pd
 import numpy as np
 import random
 import tensorflow as tf
-from SiameseModelOld import Recognizer
+from SiameseModel import Recognizer
 from matplotlib import pyplot as plt
 
 RAW_DATA_DIR = r'/work/martiks/PMData'
-PARTICIPANTS = os.listdir(RAW_DATA_DIR) # Not including the .xlsx file
-PARTICIPANTS = [p for p in PARTICIPANTS if ".xlsx" not in p]
+PARTICIPANTS = os.listdir(RAW_DATA_DIR)
+PARTICIPANTS = [p for p in PARTICIPANTS if ".xlsx" not in p] # Not including the .xlsx file
 MAX_WEEKS = 14
 CHUNK_SIZE = {
-    'heart_rate': 17280, #86400, #day=86400, week=604800
+    'heart_rate': 17280,
     'step': 10080
 }
 
@@ -21,7 +21,7 @@ DATA_TYPE = "heart_rate" # step, heart_rate
 NUM_PARTICIPANTS = 16
 assert (1 <= NUM_PARTICIPANTS <= 16), "Number of participants must be between 1-16"
 
-NUM_WEEKS = 7 # NOTE: this is actually days now, not weeks
+NUM_WEEKS = 50 # NOTE: this is actually days now, not weeks
 assert_msg = f"Number of weeks must be between 1-{MAX_WEEKS}"
 #assert (1 <= NUM_WEEKS <= MAX_WEEKS), assert_msg
 
@@ -29,7 +29,7 @@ START_WEEK = 0 # input - 1
 assert_msg = f"Must be <= {MAX_WEEKS - NUM_WEEKS}"
 #assert (START_WEEK <= (MAX_WEEKS - NUM_WEEKS)), assert_msg
 
-TEST_NUM_WEEKS = 3
+TEST_NUM_WEEKS = 20
 assert_msg = f"Must be <= {MAX_WEEKS - (START_WEEK + NUM_WEEKS)}"
 #assert (TEST_NUM_WEEKS <= (MAX_WEEKS - (START_WEEK + NUM_WEEKS))), assert_msg
 
@@ -37,6 +37,10 @@ TEST_START_WEEK = START_WEEK + NUM_WEEKS
 
 print("======================================================TEST START======================================================")
 print(f"Parameters used: num_participants={NUM_PARTICIPANTS}, num_weeks={NUM_WEEKS}, start_week={START_WEEK}, test_num_weeks={TEST_NUM_WEEKS}, test_start_week={TEST_START_WEEK}")
+
+
+# In[2]:
+
 
 def read_file(maindir, p, file):
     path = f"{maindir}/{p}/fitbit/{file}"
@@ -99,7 +103,12 @@ def clean_data_hr(df, start_date="2019-11-18 00:00:00", end_date="2020-02-23 23:
     return df
 
 
+# In[3]:
+
+
 def prepare_chunks(datasets, start_week=0, weeks=5, no_ID=False):
+    # List with participant name and corresponding week-chunks
+    # [('p01', chunks), ('p02', chunks), ...]
     chunks = []
     start_ind = start_week * CHUNK_SIZE[DATA_TYPE] # Finds index corresponding to monday 00:00:00 of start_week
     for p in datasets:
@@ -121,6 +130,73 @@ def remove_ID(dataset):
         dataset[i] = dataset[i][1]
 
 
+# In[4]:
+
+
+# BALANCED VERSION
+def balanced_sets(chunks):
+    input_1 = []
+    input_2 = []
+    # Match all chunks (balanced 50/50 split)
+    for i in range(len(chunks)):
+        same = []
+        not_same = []
+        for j in range(len(chunks)):
+            if chunks[j][0] == chunks[i][0]:
+                same.append(chunks[j])
+            else:
+                not_same.append(chunks[j])
+        random.shuffle(same)
+        random.shuffle(not_same)
+        not_same = not_same[:len(same)] # match lengths for balance
+        to_add = same + not_same
+        random.shuffle(to_add)
+        input_2.extend(to_add)
+        for x in range(len(to_add)):
+            input_1.append(chunks[i])
+    
+    return input_1, input_2
+
+
+# In[5]:
+
+
+# GENERATE LABELS AND REMOVE ID
+def generate_labels(input_1, input_2):
+    """
+    WARNING: Will modify inputs.
+    """
+    labels = []
+    for i in range(len(input_1)):
+        # Check if they have the same ID
+        p1 = input_1[i][0]
+        p2 = input_2[i][0]
+        if p1 == p2:
+            labels.append(1)
+        else:
+            labels.append(0)
+    remove_ID(input_1)
+    remove_ID(input_2)
+
+    return labels
+
+
+# In[6]:
+
+
+def prepare_inputs(start_index, length):
+    chunks = prepare_chunks(datasets, start_week=start_index, weeks=length)
+    x1, x2 = balanced_sets(chunks)
+    y = generate_labels(x1, x2)
+    X1 = np.array(x1)
+    X2 = np.array(x2)
+    Y = np.array(y)
+    
+    return X1, X2, Y
+
+
+# In[7]:
+
 
 # DATA PROCESSING
 
@@ -131,50 +207,12 @@ for p in PARTICIPANTS[:NUM_PARTICIPANTS]:
     tup = (p, d)
     datasets.append(tup)
 
-# List with participant name and corresponding week-chunks
-# [('p01', chunks_array), ('p02', chunks_array), ...]
-chunks = prepare_chunks(datasets, start_week=START_WEEK, weeks=NUM_WEEKS)
 
-# BALANCED VERSION
-input_1 = []
-input_2 = []
-labels = []
+# In[8]:
 
-# Match all chunks (balanced)
-for i in range(len(chunks)):
-    same = []
-    not_same = []
-    for j in range(len(chunks)):
-        if chunks[j][0] == chunks[i][0]:
-            same.append(chunks[j])
-        else:
-            not_same.append(chunks[j])
-    random.shuffle(same)
-    random.shuffle(not_same)
-    not_same = not_same[:len(same)] # match lengths for balance
-    to_add = same + not_same
-    random.shuffle(to_add)
-    input_2.extend(to_add)
-    for x in range(len(to_add)):
-        input_1.append(chunks[i])
 
-# GENERATE LABELS AND REMOVE ID
-for i in range(len(input_1)):
-    # Check if they have the same ID
-    p1 = input_1[i][0]
-    p2 = input_2[i][0]
-    if p1 == p2:
-        labels.append(1)
-    else:
-        labels.append(0)
-remove_ID(input_1)
-remove_ID(input_2)
-    
-X1 = np.array(input_1)
-X2 = np.array(input_2)
-Y = np.array(labels)
+X1, X2, Y = prepare_inputs(START_WEEK, NUM_WEEKS)
 
-# NEW - Separate into train and val data
 chunk_len = len(X1)
 train_len = int(chunk_len * 0.7)
 X1_train = X1[:train_len]
@@ -183,21 +221,31 @@ X2_train = X2[:train_len]
 X2_val = X2[train_len:]
 Y_train = Y[:train_len]
 Y_val = Y[train_len:]
-# END NEW
 
+
+# In[9]:
 
 
 # MODEL TRAINING
 
 recognizer = Recognizer()
 
-early_stop = tf.keras.callbacks.EarlyStopping(monitor='loss', patience=15)
+early_stop = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=10)
 
-checkpoint_filepath = '/work/martiks/saved_models/new'
-save_checkpoint = tf.keras.callbacks.ModelCheckpoint(
-    filepath=checkpoint_filepath,
+checkpoint_filepath_loss = '/work/martiks/saved_model/loss'
+save_checkpoint_loss = tf.keras.callbacks.ModelCheckpoint(
+    filepath=checkpoint_filepath_loss,
     save_weights_only=False,
     monitor='loss',
+    mode='min',
+    save_best_only=True
+)
+
+checkpoint_filepath_val_loss = '/work/martiks/saved_model/val_loss'
+save_checkpoint_val_loss = tf.keras.callbacks.ModelCheckpoint(
+    filepath=checkpoint_filepath_val_loss,
+    save_weights_only=False,
+    monitor='val_loss',
     mode='min',
     save_best_only=True
 )
@@ -205,76 +253,19 @@ save_checkpoint = tf.keras.callbacks.ModelCheckpoint(
 parameters = {
     'batch_size' : 16 ,
     'epochs' : 5000 , # 5000 if callback is implemented
-    'callbacks' : [early_stop, save_checkpoint] ,
+    'callbacks' : [early_stop, save_checkpoint_loss, save_checkpoint_val_loss] ,
     'val_data' : ([X1_val, X2_val], Y_val) #None
 }
 
 history = recognizer.fit([X1_train, X2_train], Y_train, hyperparameters=parameters)
-#recognizer.fit([X1, X2], Y, hyperparameters=parameters)
 
+
+# In[10]:
 
 
 # TESTING
 
-def calc_scores_all(shuffled_chunks, test_chunks):
-    # VERY SLOW. Checks every test_chunk against every data_chunk.
-    scores = []
-    labels = []
-    truths = []
-    for i in range(len(test_chunks)):
-        score = []
-        label = []
-        truths.append(test_chunks[i][0]) # Real label from test_chunks
-        for data_chunk in shuffled_chunks:
-            label.append(data_chunk[0])
-            test_chunk = test_chunks[i][1].reshape((1,-1))
-            data_chunk = data_chunk[1].reshape((1,-1))
-            try:
-                score.append(recognizer.predict([test_chunk, data_chunk])[0])
-            except:
-                print(i)
-                print(test_chunk)
-                print(len(test_chunks[i][1]))
-        scores.append(score)
-        labels.append(label)
-
-    results = {
-        'scores': np.array(scores),
-        'labels': np.array(labels),
-        'truths': np.array(truths)
-    }
-    
-    return results
-
-
-def calc_scores_random(shuffled_chunks, test_chunks):
-    # NB! Currently unused
-    # Checks every test_chunk against a random data_chunk.
-    scores = []
-    labels = []
-    correct = []
-    for i in range(len(test_chunks)):
-        input_1 = test_chunks[i][1].reshape((1,-1))
-        input_2 = shuffled_chunks[i][1].reshape((1,-1))
-        label = shuffled_chunks[i][0]
-        truth = test_chunks[i][0]
-        scores.append(recognizer.predict([input_1, input_2])[0])
-        labels.append(label)
-        if label == truth:
-            correct.append(1)
-        else:
-            correct.append(0)
-
-    results = {
-        'scores': np.array(scores),
-        'labels': np.array(labels),
-        'correct': np.array(correct)
-    }
-    
-    return results
-
-
-def calc_scores_new(data_chunks, test_chunks):
+def calc_scores_new(model, data_chunks, test_chunks): # is actually the old version now
     random.shuffle(data_chunks)
     random.shuffle(test_chunks)
     data_chunks = data_chunks[:len(test_chunks)] # match lengths
@@ -291,7 +282,7 @@ def calc_scores_new(data_chunks, test_chunks):
     X = np.array(data_chunks)
     Y = np.array(test_chunks)
 
-    scores = recognizer.predict([X, Y])
+    scores = model.predict([X, Y])
 
     results = {
         'scores': scores,
@@ -301,47 +292,90 @@ def calc_scores_new(data_chunks, test_chunks):
     return results
 
 
+# In[11]:
+
+
 def print_scores_new(scores, truths):
     for i in range(len(scores)):
         print(f"Test {i} ground truth: {truths[i]}, predicted score: {scores[i]}")
 
 
-def print_scores(scores, labels, truths):
-    highest = []
-    h_labels = []
-    h_correct = 0
-    for i in range( len(scores) ) :
-        index = np.argmax( scores[i] )
-        label_ = labels[i][index]
-        h_labels.append(label_)
-        highest.append(scores[i][index][0])
+# In[12]:
+
+
+def print_scores_avg(scores, truths):
+    passed = 0
+    for i in range(len(scores)):
         truth = truths[i]
-        if truth == label_:
-            h_correct += 1
-        print( 'CHUNK {} is predicted as {} (truth: {}) with confidence of {}'.format( i+1  , label_ , truth, scores[i][index][0] ) )
-    mean = np.max(np.array(highest))
-    print(f'Max confidence score: {mean}')
-    print(f'Correct predictions: {h_correct} of {len(highest)}')
+        score = np.average(scores[i])
+        test = "FAILED"
+        if (truth == 0) and (0 <= score < 0.5):
+            test = "PASSED"
+            passed += 1
+        elif (truth == 1) and (0.5 <= score <= 1):
+            test = "PASSED"
+            passed += 1
+        print(f"Test {i} ground truth: {truth}, predicted score: {score}, test: {test}")
+    print(f"Passed {passed} of {len(scores)} tests.")
 
 
-shuffled_chunks = chunks.copy()
-random.shuffle(shuffled_chunks)
-test_chunks = prepare_chunks(datasets, start_week=TEST_START_WEEK, weeks=TEST_NUM_WEEKS)
+# In[13]:
 
-#results = calc_scores_all(shuffled_chunks, test_chunks)
-#print_scores(results['scores'], results['labels'], results['truths'])
 
-results = calc_scores_new(shuffled_chunks, test_chunks)
-print_scores_new(results['scores'], results['truths'])
+def print_scores(scores, truths):
+    passed = 0
+    for i in range(len(scores)):
+        truth = truths[i]
+        score = scores[i]
+        test = "FAILED"
+        if (truth == 0) and (0 <= score < 0.5):
+            test = "PASSED"
+            passed += 1
+        elif (truth == 1) and (0.5 <= score <= 1):
+            test = "PASSED"
+            passed += 1
+        print(f"Test {i} ground truth: {truth}, predicted score: {score}, test: {test}")
+    print(f"Passed {passed} of {len(scores)} tests.")
+
+
+# In[14]:
+
+
+# Load saved best model
+loaded_recognizer = tf.keras.models.load_model('/work/martiks/saved_model/val_loss')
+
+
+# In[30]:
+
+
+# BALANCED TEST
+t1, t2, truths = prepare_inputs(TEST_START_WEEK, TEST_NUM_WEEKS)
+scores = loaded_recognizer.predict([t1, t2])
+
+
+# In[31]:
+
+
+print_scores(scores, truths)
+
+
+# In[17]:
+
 
 # plot learning curves
 plt.title('Learning Curves')
 plt.xlabel('Epoch')
 plt.ylabel('Cross Entropy')
 plt.plot(history.history['loss'], label='train')
-plt.legend()
 plt.plot(history.history['val_loss'], label='val')
 plt.legend()
-plt.savefig('learning_curve_new.png')
+plt.savefig('learning_curve.png')
 
 print("=======================================================TEST END=======================================================")
+
+
+# In[ ]:
+
+
+
+
--- SiameseModel.py
+++ SiameseModel.py
@@ -1,7 +1,7 @@
 from tensorflow.keras import models , optimizers , losses ,activations , callbacks
 from tensorflow.keras.layers import *
 import tensorflow.keras.backend as K
-from PIL import Image
+from tensorflow.keras.regularizers import l2
 import tensorflow as tf
 import time
 import os
@@ -10,89 +10,108 @@ import numpy as np
 
 class Recognizer (object) :
 
-	def __init__( self ):
+    def __init__( self ):
 
-		#input_shape = ( 10080, 1 ) # Day: 1440, Week: 10080
-		input_shape = ( 17280, 8 ) # 1-day of 5-sec frequency values
-		# ( 7, 1440 )
-		# ( 1440, 7 )
-		# increase kernel_sizes
-		kernel_size_1 = ( 32 )
-		kernel_size_2 = ( 32 )
-		pool_size_1 = ( 2 )
-		pool_size_2 = ( 2 )
-		strides = 1
+        input_shape = (17280, 8) # 1-day of 5-sec frequency values, with bpm & day-of-week features
 
-		seq_conv_model = [
+		# Define the tensors for the two input images
+        left_input = Input(input_shape)
+        right_input = Input(input_shape)
 
-			Conv1D( 32, kernel_size=kernel_size_1 , strides=strides , activation=self.leaky_relu ),
-			Conv1D( 32, kernel_size=kernel_size_1, strides=strides, activation=self.leaky_relu),
-			MaxPooling1D(pool_size=pool_size_1, strides=strides ),
+        # Convolutional Neural Network
+        model = tf.keras.Sequential()
+        model.add(Conv1D(32, (10), activation='relu',
+                         input_shape=input_shape,
+                         kernel_initializer=self.initialize_weights,
+                         kernel_regularizer=l2(2e-4)))
+        model.add(MaxPooling1D())
+        model.add(Conv1D(64, (7), activation='relu',
+                         kernel_initializer=self.initialize_weights,
+                         bias_initializer=self.initialize_bias,
+                         kernel_regularizer=l2(2e-4)))
+        model.add(MaxPooling1D())
+        model.add(Conv1D(64, (4), activation='relu',
+                         kernel_initializer=self.initialize_weights,
+                         bias_initializer=self.initialize_bias,
+                         kernel_regularizer=l2(2e-4)))
+        model.add(MaxPooling1D())
+        model.add(Conv1D(128, (4), activation='relu',
+                         kernel_initializer=self.initialize_weights,
+                         bias_initializer=self.initialize_bias,
+                         kernel_regularizer=l2(2e-4)))
+        model.add(Flatten())
+        model.add(Dense(2048, activation='sigmoid',
+                        kernel_regularizer=l2(1e-3),
+                        kernel_initializer=self.initialize_weights,
+                        bias_initializer=self.initialize_bias))
 
-			Conv1D( 64, kernel_size=kernel_size_2 , strides=strides , activation=self.leaky_relu ),
-			Conv1D( 64, kernel_size=kernel_size_2 , strides=strides , activation=self.leaky_relu ),
-			MaxPooling1D(pool_size=pool_size_2 , strides=strides),
+        # Generate the encodings (feature vectors) for the two images
+        encoded_l = model(left_input)
+        encoded_r = model(right_input)
 
-			Flatten(),
+        # Add a customized layer to compute the absolute difference between the encodings
+        L1_layer = Lambda(lambda tensors:K.abs(tensors[0] - tensors[1]))
+        L1_distance = L1_layer([encoded_l, encoded_r])
 
-			Dense( 64 , activation=activations.sigmoid ),
-            Dense( 1 , activation=activations.sigmoid )
+        # Add a dense layer with a sigmoid unit to generate the similarity score
+        prediction = Dense(1, activation='sigmoid', bias_initializer=self.initialize_bias)(L1_distance)
 
-		]
+        # Connect the inputs with the outputs
+        self.__model = models.Model(inputs=[left_input,right_input], outputs=prediction)
 
-		seq_model = tf.keras.Sequential( seq_conv_model )
+        self.__model.compile(loss="binary_crossentropy" , optimizer=optimizers.Adam(lr=0.00006), metrics=['accuracy'])
 
-		input_x1 = Input( shape=input_shape )
-		input_x2 = Input( shape=input_shape )
+    def leaky_relu(self, x):
+        return tf.nn.leaky_relu(x, alpha=0.01)
+    
+    def initialize_weights(self, shape, dtype=None):
+        """
+            The paper, http://www.cs.utoronto.ca/~gkoch/files/msc-thesis.pdf
+            suggests to initialize CNN layer weights with mean as 0.0 and standard deviation of 0.01
+        """
+        return np.random.normal(loc = 0.0, scale = 1e-2, size = shape)
+    
+    def initialize_bias(self, shape, dtype=None):
+        """
+            The paper, http://www.cs.utoronto.ca/~gkoch/files/msc-thesis.pdf
+            suggests to initialize CNN layer bias with mean as 0.5 and standard deviation of 0.01
+        """
+        return np.random.normal(loc = 0.5, scale = 1e-2, size = shape)
 
-		output_x1 = seq_model( input_x1 )
-		output_x2 = seq_model( input_x2 )
-		seq_model.summary()
-
-		distance_euclid = Lambda( lambda tensors : K.abs( tensors[0] - tensors[1] ))( [output_x1 , output_x2] )
-		outputs = Dense( 1 , activation=activations.sigmoid) ( distance_euclid )
-		self.__model = models.Model( [ input_x1 , input_x2 ] , outputs )
-
-		self.__model.compile( loss=losses.binary_crossentropy , optimizer=optimizers.Adam(lr=0.0001), metrics=['accuracy'])
-
-	def leaky_relu(self, x):
-		return tf.nn.leaky_relu(x, alpha=0.01)
-
-	def fit(self, X, Y ,  hyperparameters  ):
-		initial_time = time.time()
-		history = self.__model.fit( X  , Y ,
-						 batch_size=hyperparameters[ 'batch_size' ] ,
-						 epochs=hyperparameters[ 'epochs' ] ,
-						 callbacks=hyperparameters[ 'callbacks'],
-						 validation_data=hyperparameters[ 'val_data' ]
-						 )
-		final_time = time.time()
-		eta = ( final_time - initial_time )
-		time_unit = 'seconds'
-		if eta >= 60 :
-			eta = eta / 60
-			time_unit = 'minutes'
-		self.__model.summary( )
-		print( 'Elapsed time acquired for {} epoch(s) -> {} {}'.format( hyperparameters[ 'epochs' ] , eta , time_unit ) )
-		return history
-
-
-	def evaluate(self , test_X , test_Y  ) :
-		return self.__model.evaluate(test_X, test_Y)
-
-
-	def predict(self, X  ):
-		predictions = self.__model.predict( X  )
-		return predictions
+    def fit(self, X, Y, hyperparameters):
+        initial_time = time.time()
+        history = self.__model.fit(X, Y,
+                                   batch_size=hyperparameters['batch_size'],
+                                   epochs=hyperparameters['epochs'],
+                                   callbacks=hyperparameters['callbacks'],
+                                   validation_data=hyperparameters['val_data'])
+        final_time = time.time()
+        eta = (final_time - initial_time)
+        time_unit = 'seconds'
+        if eta >= 60 :
+            eta = eta / 60
+            time_unit = 'minutes'
+        self.__model.summary()
+        print('Elapsed time acquired for {} epoch(s) -> {} {}'.format(hyperparameters['epochs'], eta, time_unit))
+        return history
 
 
-	def summary(self):
-		self.__model.summary()
+    def evaluate(self, test_X, test_Y) :
+        return self.__model.evaluate(test_X, test_Y)
 
 
-	def save_model(self , file_path ):
-		self.__model.save(file_path )
+    def predict(self, X):
+        predictions = self.__model.predict(X)
+        return predictions
 
 
-	def load_model(self , file_path ):
-		self.__model = models.load_model(file_path)
+    def summary(self):
+        self.__model.summary()
+
+
+    def save_model(self , file_path):
+        self.__model.save(file_path)
+
+
+    def load_model(self , file_path):
+        self.__model = models.load_model(file_path)
